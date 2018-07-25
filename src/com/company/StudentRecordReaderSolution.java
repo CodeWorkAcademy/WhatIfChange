89a8c795d39471702dcf005595268b9c0a4bbae5
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-25 11:42:09 -0400

    Initial commit

---
+++ src/com/company/StudentRecordReaderSolution.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StudentRecordReaderSolution {
+    public static void main(String[] args) {
+        String fname, lname, status;
+        double gpa;
+        int hours;
+        boolean isFullTime;
+        int reply;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print( "Student's First Name? " );
+        fname = keyboard.next();
+
+        System.out.print( "Student's Last Name? " );
+        lname = keyboard.next();
+
+        System.out.print( "Student's GPA? " );
+        gpa = keyboard.nextDouble();
+
+        System.out.print( "Student's Current Course Load? " );
+        hours = keyboard.nextInt();
+
+        System.out.println("Is student full time yes(1)or no(2)");
+        reply=keyboard.nextInt();
+        if(reply==1){
+          isFullTime=true;
+        }else if(reply==2){
+            isFullTime=false;
+        }else{
+            isFullTime=false;
+        }
+
+        if(isFullTime){
+            System.out.println();
+            System.out.println("Student Name :" + fname + " " + lname);
+            System.out.println("Student GPA :" + gpa);
+            if (gpa >= 3)
+            {
+                System.out.println("This student is in good standing.");
+            } else if (gpa >= 2) {
+                System.out.println("This student needs to study more.");
+            } else if (gpa >= 1) {
+                System.out.println("This student is on academic probation.");
+            } else {
+                System.out.println("This student has been expelled.");
+            }
+        }
+    }
+}
